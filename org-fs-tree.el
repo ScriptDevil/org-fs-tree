0dd708715d423b78fed7a1cc6b7d618d74e25bed
Ashok Gautham Jadatharan <ScriptDevil@zoho.com>
2020-08-04 17:40:04 +0530

    Support limiting the number of levels using prefix arguments

--- org-fs-tree.el
+++ org-fs-tree.el
@@ -1,9 +1,9 @@
 ;;; org-fs-tree.el --- converts filesystem trees to org trees
 
-;; Copyright (C) 2015 Ashok Gautham Jadatharan
+;; Copyright (C) 2020 Ashok Gautham Jadatharan
 
 ;; Author: Ashok Gautham Jadatharan <ScriptDevil@zoho.com>
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((f "0") (names "0"))
 ;; Keywords: org-mode
 
@@ -38,28 +38,33 @@
 (defun -make-heading (s level)
   (concat (make-string level ?*) " " s "\n"))
 
-(defun -create-tree (base-dir level)
+(defun -create-tree (base-dir level limit-level)
   (let* ((full-path (f-full base-dir))
 	 (short-name  (if (f-dir? base-dir)
 			  (concat (f-filename base-dir) "/")
 			(f-filename base-dir)))
 	 (link (-make-link short-name full-path))
 	 (heading (-make-heading link level)))
-    (if (f-directory? base-dir)
-	(concat heading
-		(apply 'concat (mapcar
-				(lambda (d)  (-create-tree d (+ 1 level)))
-				(f-entries base-dir))))
-      heading)))  
+    (if (or (null limit-level) (< level limit-level))
+	(if (f-directory? base-dir)
+	    (concat heading
+		    (apply 'concat (mapcar
+				    (lambda (d)  (-create-tree d (+ 1 level) limit-level))
+				    (f-entries base-dir))))
+	  heading)
+      heading)))
 
-(defun dump (dirname)
+(defun dump (arg dirname)
   "Dump the file system tree rooted at DIRNAME as an org tree.
 Each heading in the org-tree will be a link to the corresponding
-file or directory that can be opened using org-open-at-point
+file or directory that can be opened using org-open-at-point. 
+
+Optional prefix argument can be used to limit the number of
+levels.
 "
 
-  (interactive "DDirectory to dump: ")
-  (insert (-create-tree dirname 1)))
+  (interactive "P\nDDirectory to dump: ")
+  (insert (-create-tree dirname 1 arg)))
 )
 
 (provide 'org-fs-tree)
